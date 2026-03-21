064b95d6d449752dae2beedd94da401a65005f8f
David Johnston <davidajohnston2000@gmail.com>
2026-03-21 15:32:49 +0000

    Console out for build

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -3,6 +3,7 @@ import { fileURLToPath } from "node:url";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
+console.log(__dirname);
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
