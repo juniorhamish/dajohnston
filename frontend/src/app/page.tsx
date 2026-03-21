fb5777741b60d34c859f00e7c4a0304c0ecd2546
David Johnston <davidajohnston2000@gmail.com>
2026-03-21 22:04:35 +0000

    Add a sample API invocation to the UI to test deployment

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,3 +1,4 @@
+import { ApiVersion } from "@/components/api-version";
 import { AuthButtons } from "@/components/auth-buttons";
 
 export default async function Home() {
@@ -14,6 +15,7 @@ export default async function Home() {
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
         <p>&copy; {new Date().getFullYear()} uk.co.dajohnston</p>
+        <ApiVersion />
       </footer>
     </div>
   );
