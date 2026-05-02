23104197554a3abb637f7abfbc4b81345e24a834
David Johnston <davidajohnston2000@gmail.com>
2026-05-02 21:52:08 +0100

    Sonar fixes

--- backend/portal/src/main/java/uk/co/dajohnston/portal/config/TenantSessionAspect.java
+++ backend/portal/src/main/java/uk/co/dajohnston/portal/config/TenantSessionAspect.java
@@ -7,8 +7,6 @@ import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
-import org.springframework.transaction.support.TransactionSynchronization;
-import org.springframework.transaction.support.TransactionSynchronizationManager;
 import uk.co.dajohnston.security.context.TenantContext;
 
 @Aspect
@@ -23,21 +21,6 @@ public class TenantSessionAspect {
       "@within(org.springframework.transaction.annotation.Transactional) "
           + "|| @annotation(org.springframework.transaction.annotation.Transactional)")
   public void setTenantSessionVariable() {
-    if (TransactionSynchronizationManager.isActualTransactionActive()) {
-      if (TransactionSynchronizationManager.getResource(this) != null) {
-        return;
-      }
-      TransactionSynchronizationManager.bindResource(this, Boolean.TRUE);
-      TransactionSynchronizationManager.registerSynchronization(
-          new TransactionSynchronization() {
-            @Override
-            public void afterCompletion(int status) {
-              TransactionSynchronizationManager.unbindResourceIfPossible(TenantSessionAspect.this);
-            }
-          });
-    }
-
-    long start = System.currentTimeMillis();
     UUID userId = TenantContext.getUserId();
     String email = TenantContext.getUserEmail();
     UUID tenantId = TenantContext.getTenantId();
@@ -48,15 +31,24 @@ public class TenantSessionAspect {
         email,
         tenantId);
 
+    var userIdString = "";
+    if (userId != null) {
+      userIdString = userId.toString();
+    }
+    if (email == null) {
+      email = "";
+    }
+    var tenantIdString = "";
+    if (tenantId != null) {
+      tenantIdString = tenantId.toString();
+    }
+
     jdbcTemplate.queryForList(
         "SELECT set_config('app.current_user_id', ?, true), "
             + "set_config('app.current_user_email', ?, true), "
             + "set_config('app.current_household_id', ?, true)",
-        userId != null ? userId.toString() : "",
-        email != null ? email : "",
-        tenantId != null ? tenantId.toString() : "");
-
-    log.debug(
-        "Completed setting tenant session variables in [{}ms]", System.currentTimeMillis() - start);
+        userIdString,
+        email,
+        tenantIdString);
   }
 }
